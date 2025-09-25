5a168b19be9baa585c6296091385296a6b54a551
Student <nguyenanthonyd4@gmail.com>
2025-09-25 10:30:36 -0700

    workbook practice  module 3 exercise 3 pt 1

--- RecapFunDay4/src/main/java/com/pluralsight/Main.java
+++ RecapFunDay4/src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
 
     String movieTitle = "Pokemon The Movie 2000";
