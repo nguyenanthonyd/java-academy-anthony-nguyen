d5df16a1e25a27c5cb2619f3acdb1281d7dea860
Student <nguyenanthonyd4@gmail.com>
2025-09-25 11:08:03 -0700

    ShoppingList

---
+++ ShoppingList/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("1.Apples 2.Blueberries 3.Strawberries 4.Bananas 5.Dragonfruit 6.Honey Bunches 7.Milk ");
+
+        System.out.printf(" 8.Water 9.Chips 10.Bread.");
+
+        System.out.printf(" Don't forget your tote!");
+
+
+
+
+
+    }
+
+}
