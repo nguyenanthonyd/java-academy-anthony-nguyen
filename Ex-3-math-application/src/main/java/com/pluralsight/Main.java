c9b34d600b0c4094cfbeb7666835756470ec7e3c
Student <nguyenanthonyd4@gmail.com>
2025-09-25 08:25:33 -0700

    workbook practice  module 3 exercise 3 pt 1

---
+++ Ex-3-math-application/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+ double bobSalary = 45000.00;
+         double garySalary = 50000.00;
+                 double highestSalary =  Math.max(bobSalary, garySalary);
+        System.out.println( "Bob's Salary:$" + bobSalary);
+        System.out.println( "Gary's Salary:$" + garySalary);
+        System.out.println( "Highest Salary:$" + highestSalary);
+
+
+
+    }
+}
---
+++ ex -3/src/Main/java/com/pluralsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String item = "Coffee"; // item name
+        double itemPrice = 34.99; // price per item
+        int coffee = 3; // quantity
+
+        System.out.println("I bought " + coffee + " " + item + " for " + "$" + itemPrice + "!!!");
+    }
+
+    }
