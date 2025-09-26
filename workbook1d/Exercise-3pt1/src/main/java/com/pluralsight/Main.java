4a74fbfb7b0d6b7c35f4bae9fe61457c75b28e74
Student <nguyenanthonyd4@gmail.com>
2025-09-25 17:26:29 -0700

    safe 2

---
+++ workbook1d/Exercise-3pt1/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Salary without dollars
+        // Determining whose salary is greater
+
+
+        double bobSalary = 40000.00;
+        double garySalary = 50000.00;
+        double highestSalary = Math.max(bobSalary, garySalary); // Determined salary using mathmax
+
+
+        System.out.println("The highest salary is:");
+        System.out.println("Bob's Salary:$" + bobSalary);
+        System.out.println("Gary's Salary:$" + garySalary);
+        System.out.println("The highest Salary is:$" + highestSalary);
+
+        // results
+
+
+
+    }
+}
