a7cf2e5e4e6d2e28a1eedaf9786133b14cb7284b
Student <nguyenanthonyd4@gmail.com>
2025-09-25 17:59:33 -0700

    Exercise 2 - Step 1

---
+++ workbook1d/Exercise-2-pt2/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+// Exercise 2 Step:1
+        // Print each variable with a descriptive message
+
+
+        String favoriteColor = "Black";
+
+        String yearStarted = "2025";
+
+        char middleInitial = 'D';
+
+        boolean havePets = true;
+
+        String niceMessage = " Life is good!";
+
+
+        System.out.printf("Hi, I'm Anthony. My favorite color is " + favoriteColor + ".");
+
+        System.out.printf(" My middle initial is " + middleInitial + " for " + "Duy." );
+
+        System.out.printf(" I started YearUpUnited in " + yearStarted + ".");
+
+        if (havePets);
+        {
+            System.out.printf(" I have a dog named Teddy.");
+        }
+        System.out.println(niceMessage);
+
+    }
+
+
+}
