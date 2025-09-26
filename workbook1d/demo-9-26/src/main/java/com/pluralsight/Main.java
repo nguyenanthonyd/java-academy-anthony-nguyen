dbc08a06fb28e4f3732e1168cf367734337a7f41
Student <nguyenanthonyd4@gmail.com>
2025-09-26 13:37:45 -0700

    Password Demo without Scanner

---
+++ workbook1d/demo-9-26/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String userPassword = "1234";
+
+        if (userPassword.equals ("1234")) {
+
+            System.out.println("Correct password!");
+
+
+
+        } else {
+
+            System.out.println("Wrong password!");
+        }
+    }
+
+    }
