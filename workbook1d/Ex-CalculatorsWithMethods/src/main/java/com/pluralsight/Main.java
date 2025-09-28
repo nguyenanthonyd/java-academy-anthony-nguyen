742a82789e05234c79ce306748fdbd9bdbf1a2a7
Student <nguyenanthonyd4@gmail.com>
2025-09-28 14:00:00 -0700

    MethodPractice

---
+++ workbook1d/Ex-CalculatorsWithMethods/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // TODO: call sayHello();
+        // TODO: call sayGoodbye();
+        // TODO: call sayGoodMorning();
+
+    }
+
+    public static void sayHello () {
+        // TODO: print: Hello, World!
+    }
+
+    public static void sayGoodbye() {
+        // TODO: print: Goodbye!
+    }
+    public static void sayGoodMorning() {
+        // TODO: print: Good morning!
+
+    }
+}
