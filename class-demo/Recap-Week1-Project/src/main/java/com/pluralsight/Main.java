9d3df6d4030abccff4ad878f521c6b57c8948800
Student <nguyenanthonyd4@gmail.com>
2025-09-29 09:54:49 -0700

    in class demo

---
+++ class-demo/Recap-Week1-Project/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+
+
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter exam score (0-100):");
+        int examScore = sc.nextInt(); // Stores input into a variable
+
+        char grade;
+
+        if (examScore >= 90 && examScore <= 100) {
+            grade = 'A';
+
+        }else if (examScore >= 80) {
+            grade = 'B';
+
+        }else if (examScore >= 70) {
+            grade = 'C';
+
+        } else if (examScore >= 60) {
+            grade = 'D';
+
+        } else {
+                grade = 'F';
+        }
+
+
+            System.out.println("Your grade: " + grade);
+
+        }
+
+}
