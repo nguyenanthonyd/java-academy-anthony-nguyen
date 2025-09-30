3be76433bbb57192a3d321790f2dd3e338fdd0bd
Student <nguyenanthonyd4@gmail.com>
2025-09-30 08:49:36 -0700

    Exercise 1

---
+++ class-demo/LoopsAndArrays-ClassDemo/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+         int i = 1;
+         do {
+             System.out.println("I will be a good developer;");
+             i++;
+         } while (i<=2);
+         }
+    }
