e4d653ebeb9031910a8e3b5ba0292196ee0f6a7e
Student <nguyenanthonyd4@gmail.com>
2025-10-03 21:25:56 -0700

    Looping Exercise 1 Step 1 &2

---
+++ workbook 2a/LoopingExercise/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        do {
+            System.out.println("I love Java");
+            counter++;
+        } while (counter < 5);
+        }
+    }
+
+
+
