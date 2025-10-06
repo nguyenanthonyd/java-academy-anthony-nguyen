2f3e50ab7c75169907d1011bc7d52f7999f79142
Student <nguyenanthonyd4@gmail.com>
2025-10-06 12:19:09 -0700

    in class debugging

---
+++ workBook 2b/CarInventory/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+    // Fields for now
+
+    private int id;
+    private String makeModel;
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity; // in liters
+    private int fuelCapacity; // in liters
+    private String vin;
+
+
+
+
+}
+}
+
