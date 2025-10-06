2a303f26d8393324c040f06d950c8be911ce4824
Student <nguyenanthonyd4@gmail.com>
2025-10-06 14:48:01 -0700

    Working with IntelliJ

--- workBook 2b/CarInventory/src/main/java/com/pluralsight/Vehicle.java
+++ workBook 2b/CarInventory/src/main/java/com/pluralsight/Vehicle.java
@@ -12,9 +12,80 @@ public class Vehicle {
     private int fuelCapacity; // in liters
     private String vin;
 
+    public Vehicle(int id, String makeModel, String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity, String vin) {
+        this.id = id;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.numberOfPassengers = numberOfPassengers;
+        this.cargoCapacity = cargoCapacity;
+        this.fuelCapacity = fuelCapacity;
+        this.vin = vin;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "id=" + id +
+                ", makeModel='" + makeModel + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
 }
-}
+
 
