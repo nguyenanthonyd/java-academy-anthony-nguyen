3fdb999da28575a37d4b7473b17b36d92b0ec7da
Student <nguyenanthonyd4@gmail.com>
2025-10-09 19:17:03 -0700

    FileReader loaded

--- workbook 3a/PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ workbook 3a/PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -31,3 +31,28 @@ public class Main {
 
 }
 
+//Exercises — Payroll Calculator Pt. 2
+//EXERCISE 1
+//Continue working on the PayrollCalculator program.
+//Rather than displaying your payroll report to the screen, write it to a .csv file in
+//the following format.
+//id|name|gross pay
+//111|Cameron Tay|3277.65
+//222|James Tee|2150.00
+//Prompt the user for the name of a file to read and process, then prompt them for
+//the name of the payroll file to create.
+//Enter the name of the file employee file to process: employees.csv
+//Enter the name of the payroll file to create: payroll-sept-2023.csv
+//When your program finishes running, open the new file to view the results.
+//BONUS: If the user chooses specifies a .json extension write the data as JSON
+//instead of csv.
+//For example:
+//Enter the name of the file employee file to process: employees.csv
+//Enter the name of the payroll file to create: payroll-sept-2023.json
+//payroll-sept-2023.json
+//[
+//{ "id": 111, "name" : "Cameron Tay", "grossPay" : 3277.65 },
+//{ "id": 222, "name" : "James Tee", "grossPay" : 2150.00 }
+//]
+//1. Ensure all your changes are committed and pushed to GitHub
+//2. Let your Instructor know you’ve completed this exercise
