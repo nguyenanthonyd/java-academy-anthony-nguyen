2a004b555d228564e93c4bd589b63e7a85a3ab4b
Student <nguyenanthonyd4@gmail.com>
2025-10-15 17:28:35 -0700

    added methods and updated Strings

--- Capstones/Capstone-Mo-Money-No-Problems/src/main/java/com/pluralsight/HomeScreen.java
+++ Capstones/Capstone-Mo-Money-No-Problems/src/main/java/com/pluralsight/HomeScreen.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class HomeScreen {
@@ -7,39 +9,64 @@ public class HomeScreen {
     public static void homeScreen() {
 
         // Run the homeScreen in a do- while loop until the user exits the menu.
+        TransactionFileHelper transactionFileHelper = new TransactionFileHelper();
         Scanner scanner = new Scanner(System.in);
         char choice;
 
         do {
-            System.out.println("\n$$$ Mo Money No Problems Menu $$$");
+            System.out.println("\n===$$$ Mo' Money No Problems Menu $$$===");
             System.out.println("D) Add Deposit");
             System.out.println("P) Make Payment (Debit)");
             System.out.println("L) Ledger - display the ledger screen");
             System.out.println("X) Exit ");
 
             choice = scanner.nextLine().toUpperCase().charAt(0);
-            // Chain of method that reads user input from the keyboard,
-            // Converts to uppercase and takes the first character.
+            // Chain of method that reads user input from the keyboard
+            // Converts to uppercase and takes the first character
 
             switch (choice) {
                 case 'D':
                     System.out.println("Add Deposit selected");
+                    LocalDate date = LocalDate.now();
+                    LocalTime time = LocalTime.now();
+                    System.out.println("Enter description");
+                    String description = scanner.nextLine();
+                    System.out.println("Enter vendor");
+                    String vendor = scanner.nextLine();
+                    System.out.println("Enter amount");
+                    double amount = Double.parseDouble(scanner.nextLine());
+                    String transaction = date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+                    transactionFileHelper.writeTransaction(transaction);
+                    //date|time|description|vendor|amount
                     break;
-
+            //  public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount)
                 case 'P':
                     System.out.println("Make Payment selected");
+                    LocalDate dateP = LocalDate.now();
+                    LocalTime timeP = LocalTime.now();
+                    System.out.println("Enter description");
+                    String descriptionP = scanner.nextLine();
+                    System.out.println("Enter vendor");
+                    String vendorP = scanner.nextLine();
+                    System.out.println("Enter amount");
+                    double amountP = -1 * Double.parseDouble(scanner.nextLine());
+                    String transactionP = dateP + "|" + timeP + "|" + descriptionP + "|" + vendorP + "|" + amountP;
+                    transactionFileHelper.writeTransaction(transactionP);
+                    //date|time|description|vendor|amount
                     break;
 
                 case 'L':
-                    System.out.println("Cruisin' to the ledger screen");
+                    System.out.println("Cruisin' to the ledger screen...");
                     // switches to ledger page with different options
+                    LedgerScreen.ledgerScreen();
                     break;
 
                 case 'X':
                     System.out.println("Peace!");
                     break;
-                default:
-                    System.out.println("Oh snap, invalid choice! Please try again!");
+
+                    default:
+                    System.out.println("Oh snap, invalid choice! Try that again playa!");
 
             }
 
