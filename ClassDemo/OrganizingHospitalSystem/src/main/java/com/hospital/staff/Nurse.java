036ea7d44f62911f18da414b4ed2a0072ac6e123
Student <nguyenanthonyd4@gmail.com>
2025-10-23 09:26:24 -0700

    save

--- ClassDemo/OrganizingHospitalSystem/src/main/java/com/hospital/staff/Nurse.java
+++ ClassDemo/OrganizingHospitalSystem/src/main/java/com/hospital/staff/Nurse.java
@@ -5,7 +5,7 @@ public class Nurse extends StaffMember {
     public Nurse(String name) {
         super(name);
     }
-
+//
     @Override
     public void work() {
         System.out.println(name + " is preparing medication...");
