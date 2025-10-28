0bd734c3bdcaa45ac404d10f8aaf72420a394632
Student <nguyenanthonyd4@gmail.com>
2025-10-28 10:40:39 -0700

    exercises

---
+++ ClassDemo/InheritanceExercise/src/main/java/com/pluralsight/Chemist.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class Chemist {
+    public void cook() {
+        System.out.println(" Cooking something ordinary.");
+    }
+}
---
+++ ClassDemo/InheritanceExercise/src/main/java/com/pluralsight/WalterWhite.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.sql.SQLOutput;
+
+public class WalterWhite extends Chemist  {
+
+    @Override
+    public void cook() {
+        System.out.println("I am the one who knocks!");
+
+    }
+}
